6da0606e6512aa8f16defeb2e0356d8fcd848a94
Matthias Frey <mfrey43@gmail.com>
2018-04-27 13:09:11 +0200

    fix

--- src/main/java/ch/tie/springboottemplate/TemplateApplication.java
+++ src/main/java/ch/tie/springboottemplate/TemplateApplication.java
@@ -3,19 +3,27 @@ package ch.tie.springboottemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 import com.zaxxer.hikari.HikariDataSource;
 
+import ch.tie.spring.AppContext;
 import ch.tie.spring.SpringConfiguration;
 
 @SpringBootApplication
 @Import({SpringConfiguration.class})
-public class TemplateApplication {
+public class TemplateApplication implements ApplicationContextAware {
 
   public static void main(String[] args) {
     SpringApplication.run(TemplateApplication.class, args);
+  }
+
+  @Override
+  public void setApplicationContext(ApplicationContext ctx) {
+    AppContext.setApplicationContext(ctx);
   }
 
   @Bean(name = "apiUserDs")
